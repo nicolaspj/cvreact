a5e8523d3e4c1914af5380810906c02cd23dca15
Nicolas C <cardozo.j.nicolas@gmail.com>
2025-02-17 10:33:38 -0300

    cambios de div + degrade de divs

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -1,60 +1,82 @@
 function Aboutme() {
   return (
-    <div class="container ">
+    <div className="container">
       <div>
-        <p class="fs-3 fst-italic text-center">
-          Soy un desarrollador Backend PHP con 3 años de experiencial en el
-          campo donde desarrollo y mantengo aplicaciones web a medida del
-          cliente,además de contar con conocimientos básicos de Frontend y
-          estudiando actualmente React para futuras implementaciones.
-        </p>
+        <div className="bg-dark bg-opacity-50 p-3 mb-3 rounded">
+          <p className="fs-3 fst-italic text-center text-light">
+            Soy un desarrollador Backend PHP con 3 años de experiencia en el
+            campo donde desarrollo y mantengo aplicaciones web a medida del
+            cliente, además de contar con conocimientos básicos de Frontend y
+            estudiando actualmente React para futuras implementaciones.
+          </p>
+        </div>
 
-        <p class="fs-1 fw-bolder text-center  ">
-          EXPERIENCIA Y RESPONSABILIDADES
-        </p>
+        <div className="bg-dark bg-opacity-50 p-3 mb-3 rounded">
+          <p className="fs-1 fw-bolder text-center text-light">
+            EXPERIENCIA Y RESPONSABILIDADES
+          </p>
+        </div>
 
-        <p class="fs-2 fw-semibold text-center ">
-          Migración de Bases de Datos:
-        </p>
-        <p class="fs-4 text-center">
-          Coordinación y ejecución de procesos de migración de bases de datos
-          entre distintas plataformas, asegurando la integridad de los datos y
-          la mínima interrupción en los servicios. Implementación de
-          procedimientos para garantizar la consistencia y el rendimiento óptimo
-          en el nuevo entorno.
-        </p>
+        <div className="bg-dark bg-opacity-50 p-3 mb-3 rounded">
+          <p className="fs-2 fw-semibold text-center text-light">
+            Migración de Bases de Datos:
+          </p>
+          <p className="fs-4 text-center text-light">
+            Coordinación y ejecución de procesos de migración de bases de datos
+            entre distintas plataformas, asegurando la integridad de los datos y
+            la mínima interrupción en los servicios. Implementación de
+            procedimientos para garantizar la consistencia y el rendimiento
+            óptimo en el nuevo entorno.
+          </p>
+        </div>
 
-        <p class="fs-2 fw-semibold text-center ">
-          Actualización de PHP (Versión 5.6 a 8.1):
-        </p>
-        <p class="fs-4 text-center">
-          Realize actualizaciones de aplicaciones y sistemas de PHP de la
-          versión 5.6 a la 8.1, solucionando incompatibilidades de código,
-          optimizando el rendimiento y asegurando la correcta ejecución de los
-          proyectos bajo la nueva versión del lenguaje.
-        </p>
-        <p class="fs-2 fw-semibold text-center">Migración de Servidores:</p>
-        <p class="fs-4 text-center">
-          Planificación y ejecución de la migración de servidores, con el
-          objetivo de mejorar la escalabilidad y rendimiento de los sistemas.
-        </p>
-        <p class="fs-2 fw-semibold text-center">Soporte al Cliente:</p>
-        <p class="fs-4 text-center">
-          Provisión de soporte técnico a clientes, abordando consultas,
-          resolviendo incidencias y ofreciendo soluciones a medida. Fomento de
-          la satisfacción del cliente mediante un servicio ágil y profesional.
-        </p>
-        <p class="fs-2 fw-semibold text-center">
-          Desarrollo a Medida para Clientes:
-        </p>
-        <p class="fs-4 text-center">
-          Diseño y desarrollo de soluciones personalizadas para clientes,
-          adaptando funcionalidades a sus necesidades específicas.
-          Implementación de nuevas características y mejoras, mejorando la
-          eficiencia y la experiencia del usuario.
-        </p>
+        <div className="bg-dark bg-opacity-50 p-3 mb-3 rounded">
+          <p className="fs-2 fw-semibold text-center text-light">
+            Actualización de PHP (Versión 5.6 a 8.1):
+          </p>
+          <p className="fs-4 text-center text-light">
+            Realicé actualizaciones de aplicaciones y sistemas de PHP de la
+            versión 5.6 a la 8.1, solucionando incompatibilidades de código,
+            optimizando el rendimiento y asegurando la correcta ejecución de los
+            proyectos bajo la nueva versión del lenguaje.
+          </p>
+        </div>
+
+        <div className="bg-dark bg-opacity-50 p-3 mb-3 rounded">
+          <p className="fs-2 fw-semibold text-center text-light">
+            Migración de Servidores:
+          </p>
+          <p className="fs-4 text-center text-light">
+            Planificación y ejecución de la migración de servidores, con el
+            objetivo de mejorar la escalabilidad y rendimiento de los sistemas.
+          </p>
+        </div>
+
+        <div className="bg-dark bg-opacity-50 p-3 mb-3 rounded">
+          <p className="fs-2 fw-semibold text-center text-light">
+            Soporte al Cliente:
+          </p>
+          <p className="fs-4 text-center text-light">
+            Provisión de soporte técnico a clientes, abordando consultas,
+            resolviendo incidencias y ofreciendo soluciones a medida. Fomento de
+            la satisfacción del cliente mediante un servicio ágil y profesional.
+          </p>
+        </div>
+
+        <div className="bg-dark bg-opacity-50 p-3 mb-3 rounded">
+          <p className="fs-2 fw-semibold text-center text-light">
+            Desarrollo a Medida para Clientes:
+          </p>
+          <p className="fs-4 text-center text-light">
+            Diseño y desarrollo de soluciones personalizadas para clientes,
+            adaptando funcionalidades a sus necesidades específicas.
+            Implementación de nuevas características y mejoras, mejorando la
+            eficiencia y la experiencia del usuario.
+          </p>
+        </div>
       </div>
     </div>
   );
 }
+
 export default Aboutme;
