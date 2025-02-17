ace8056284346e2318e29bca9e89c6fbe00fec61
Nicolas C <cardozo.j.nicolas@gmail.com>
2025-02-17 11:02:17 -0300

    cambios de div

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,10 +1,10 @@
 function Skills() {
   return (
-    <div class="container ">
-      <div class="grid text-center">
-        <p class="d-inline-flex gap-1">
+    <div className="container ">
+      <div className="grid text-center">
+        <p className="d-inline-flex gap-1">
           <button
-            class="btn btn-primary"
+            className="btn btn-primary"
             data-bs-toggle="collapse"
             href="#multiCollapseExample1"
             role="button"
@@ -14,7 +14,7 @@ function Skills() {
             Contactos
           </button>
           <button
-            class="btn btn-primary"
+            className="btn btn-primary"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#multiCollapseExample2"
@@ -24,7 +24,7 @@ function Skills() {
             Skills Backend
           </button>
           <button
-            class="btn btn-primary"
+            className="btn btn-primary"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#multiCollapseExample3"
@@ -34,7 +34,7 @@ function Skills() {
             Skills Frontend
           </button>
           <button
-            class="btn btn-primary"
+            className="btn btn-primary"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#multiCollapseExample4"
@@ -44,7 +44,7 @@ function Skills() {
             Estudios
           </button>
           <button
-            class="btn btn-primary"
+            className="btn btn-primary"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#multiCollapseExample5"
@@ -54,7 +54,7 @@ function Skills() {
             Idiomas
           </button>
           <button
-            class="btn btn-primary"
+            className="btn btn-primary"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target=".multi-collapse"
@@ -64,78 +64,71 @@ function Skills() {
             Mostrar todo
           </button>
         </p>
-        <div class="row">
-          <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample1">
-              <div class="card card-body">
-                <pre>
-                  <i class="bi bi-envelope-at-fill"></i>
-                  cardozo.j.nicolas@gmail.com
-                </pre>
-                <pre>
-                  <i class="bi bi-linkedin">
+        <div className="row">
+          <div className="col">
+            <div className="collapse multi-collapse" id="multiCollapseExample1">
+              <div className="card card-body">
+                <i className="bi bi-envelope-at-fill ms-3"></i>
+                <p>cardozo.j.nicolas@gmail.com</p>
+                <p>
+                  <i className="bi bi-linkedin ms-3">
                     <a href="https://www.linkedin.com/in/nicolas-cardozo-419631192/">
                       https://www.linkedin.com/in/nicolas-cardozo-419631192/
                     </a>
                   </i>
-                </pre>
+                </p>
               </div>
             </div>
           </div>
-          <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample2">
-              <div class="card card-body">
-                <pre>
-                  <p class="text-center">
-                    <i class="bi bi-pc-display"></i>
-                    Skills Backend
-                  </p>
-                  <p class="text-center">
-                    PHP , JS, SQL , MYSQL ,GIT, <br />
-                    POSTMAN,APACHE,LINUX, <br />
-                    CONTINUO APRENDIZAJE
-                    <br />
-                  </p>
-                </pre>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample3">
-              <div class="card card-body">
-                <p p class="text-center">
-                  <i class="bi bi-pc-display"></i>Skills Frontend
-                  <br />
-                  Html ,css , JS , bootstrap, <br />
-                  continuo aprendizaje
+          <div className="col">
+            <div className="collapse multi-collapse" id="multiCollapseExample2">
+              <div className="card card-body">
+                <i className="bi bi-pc-display"></i>
+                <p className="text-center">Skills Backend</p>
+                <p className="text-center">
+                  PHP , JS, SQL , MYSQL ,GIT, <br />
+                  POSTMAN,APACHE,LINUX, <br />
+                  CONTINUO APRENDIZAJE
                   <br />
                 </p>
               </div>
             </div>
           </div>
-          <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample4">
-              <div class="card card-body">
-                <p class="text-center">
-                  <i class="bi bi-eyeglasses"></i>.Estudios
-                  <pre>PHP arquitectura avanzada(Educaci&#243;n IT)</pre>
-                  <pre>
-                    .Programaci&#243;n web con PHP y Mysql (Educacion IT)
-                  </pre>
-                  <pre>.Linux Sysadmin(Educacion IT)</pre>
-                  <pre>.Programaci&#243;n Web con Javascript(UTN FRRe)</pre>
-                </p>
+          <div className="col">
+            <div className="collapse multi-collapse" id="multiCollapseExample3">
+              <div className="card card-body">
+                <div className="text-center">
+                  <i className="bi bi-pc-display"></i>
+                  <p className="text-center">Skills Frontend</p>
+                  <p>
+                    Html ,css , JS , bootstrap, <br />
+                    continuo aprendizaje
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-          <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample5">
-              <div class="card card-body">
-                <p class="text-center">
-                  <i class="bi bi-megaphone"></i>Idiomas
-                  <br />
+          <div className="col">
+            <div className="collapse multi-collapse" id="multiCollapseExample4">
+              <div className="card card-body">
+                <div className="text-center">
+                  <i className="bi bi-eyeglasses"></i>
+                  <p>Estudios</p>
+                  <p>PHP arquitectura avanzada(Educaci&#243;n IT)</p>
+                  <p>.Programaci&#243;n web con PHP y Mysql (Educacion IT)</p>
+                  <p>.Linux Sysadmin(Educacion IT)</p>
+                  <p>.Programaci&#243;n Web con Javascript(UTN FRRe)</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="collapse multi-collapse" id="multiCollapseExample5">
+              <div className="card card-body">
+                <p className="text-center">
+                  <i className="bi bi-megaphone"></i>
+                  <p>Idiomas</p>
                   Ingl&#233;s: Escritura y Lectura - B2
-                  <br />
                 </p>
               </div>
             </div>
